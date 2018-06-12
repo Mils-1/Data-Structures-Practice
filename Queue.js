b137814eb8c0d227304b7177a870440d6159b5a3
Nick Milazzo <nmils820@gmail.com>
2018-06-12 16:21:30 -0400

    Finished Queue.js

--- Queue.js
+++ Queue.js
@@ -2,14 +2,19 @@
 
 class Queue {
   constructor () {
-
+    this.data = [];
+    this.head = 0;
+    this.tail = 0;
   }
 
-  add () {
-
+  add (item) {
+    this.data[this.tail] = item;
+    this.tail++;
+    return this;
   }
 
   remove () {
-
+    if (this.head === this.tail) return;
+    return this.data[this.head++];
   }
 }
